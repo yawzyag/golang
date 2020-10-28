7324109bcc89d776239a1ca9700d2e4c75246a96
yawzyag <yesid.dev93@gmail.com>
2020-10-28 22:08:42 +0000

    [Feature] hello world

--- hello.go
+++ hello.go
@@ -1,12 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const spanish = "spanish"
+const french = "french"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello world string
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return getPrefix(lang) + name
+}
+
+// getPrefix acording to languaje
+func getPrefix(lang string) (prefix string) {
+	switch strings.ToLower(lang) {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("yesid"))
+	fmt.Println(Hello("yesid", ""))
 }
